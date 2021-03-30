35a84090ad806ef4af61b25f73e722e00f084153
Genchi Lu <genchi.lu@TW168020131M.local>
2021-03-30 13:54:32 +0800

    Add memory impl of user repo

--- persistence/src/main/java/hk/onedegree/persistence/MemUserRepository.java
+++ persistence/src/main/java/hk/onedegree/persistence/MemUserRepository.java
@@ -1,2 +1,31 @@
-package hk.onedegree.persistence;public class MemUserRepository {
+package hk.onedegree.persistence;
+
+import hk.onedegree.domain.auth.aggregates.user.User;
+import hk.onedegree.domain.auth.repository.UserRepository;
+
+import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class MemUserRepository implements UserRepository {
+    ConcurrentHashMap<String, User> emailUserMap = new ConcurrentHashMap<>();
+
+    private static MemUserRepository instance = new MemUserRepository();
+    public static MemUserRepository getInstance(){
+        return instance;
+    }
+
+    @Override
+    public Optional<User> findByEmail(String email) {
+        return Optional.ofNullable(this.emailUserMap.get(email));
+    }
+
+    @Override
+    public void save(User user) {
+        this.emailUserMap.put(user.getEmail(), user);
+    }
+
+    @Override
+    public void delete(User user) {
+        this.emailUserMap.remove(user.getEmail());
+    }
 }
