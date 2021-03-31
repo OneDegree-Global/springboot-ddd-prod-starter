12ae2b73acc096a1a3be1240598567404c1808b6
Genchi Lu <genchi.lu@TW168020131M.local>
2021-03-31 18:03:42 +0800

    Add unit test

--- domain/src/main/java/hk/onedegree/domain/auth/services/AuthTokenService.java
+++ domain/src/main/java/hk/onedegree/domain/auth/services/AuthTokenService.java
@@ -7,9 +7,13 @@ import com.nimbusds.jose.crypto.RSASSAVerifier;
 import com.nimbusds.jose.crypto.ECDSAVerifier;
 import com.nimbusds.jose.jwk.JWK;
 import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.source.JWKSource;
+import com.nimbusds.jose.proc.*;
 import com.nimbusds.jwt.JWTClaimsSet;
 import com.nimbusds.jwt.SignedJWT;
+import com.nimbusds.jwt.proc.*;
 import hk.onedegree.domain.auth.aggregates.user.User;
+import hk.onedegree.domain.auth.exceptions.InValidEmailException;
 import hk.onedegree.domain.auth.exceptions.UnsupportedAlgException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -18,14 +22,24 @@ import javax.inject.Inject;
 import java.text.ParseException;
 import java.time.*;
 import java.time.temporal.ChronoUnit;
+import java.util.Arrays;
 import java.util.Date;
+import java.util.HashSet;
+import java.util.Optional;
 
 class AuthTokenService {
 
     @Inject
     JWKSet jwkSet;
 
+    @Inject
+    JWKSource<SecurityContext> jwkSource;
+
     Clock clock = Clock.systemUTC();
+
+    private JWTClaimsSetVerifier jwtClaimsVerifier = new DefaultJWTClaimsVerifier(
+            new JWTClaimsSet.Builder().issuer(this.issuer).build(),
+            new HashSet<>(Arrays.asList("sub", "iat", "exp")));
 
     private static Logger logger = LoggerFactory.getLogger(AuthTokenService.class);
 
@@ -38,15 +52,41 @@ class AuthTokenService {
         return false;
     }
 
-    public boolean validateJWT(String jwtStr) {
+    public Optional<User> getUserFromJWT(String jwtStr) {
         SignedJWT signedJWT = null;
         try {
             signedJWT = SignedJWT.parse(jwtStr);
         } catch (ParseException e) {
             logger.error("Parse jwt str error, exception: ", e);
-            return false;
+            return Optional.empty();
         }
 
+//        if(!verifySign(signedJWT)) {
+//            return Optional.empty();
+//        }
+
+        ConfigurableJWTProcessor<SecurityContext> jwtProcessor = new DefaultJWTProcessor<>();
+
+        JWSKeySelector<SecurityContext> jwsKeySelector =
+                new JWSVerificationKeySelector<>(signedJWT.getHeader().getAlgorithm(), jwkSource);
+
+        jwtProcessor.setJWSKeySelector(jwsKeySelector);
+        jwtProcessor.setJWTClaimsSetVerifier(this.jwtClaimsVerifier);
+
+        try {
+            JWTClaimsSet claimsSet = jwtProcessor.process(signedJWT,  null);
+            User user = new User(
+                    claimsSet.getSubject(),
+                    claimsSet.getClaim("email").toString());
+
+            return Optional.of(user);
+        } catch (BadJOSEException | JOSEException | InValidEmailException e) {
+            logger.error("Get user from claim set in jwt fails, e: ", e);
+            return Optional.empty();
+        }
+    }
+
+    private boolean verifySign(SignedJWT signedJWT ) {
         JWK jwk = this.jwkSet.getKeyByKeyId(signedJWT.getHeader().getKeyID());
         if (jwk==null) {
             logger.error("Kid {} not found.", signedJWT.getHeader().getKeyID());
@@ -91,7 +131,8 @@ class AuthTokenService {
                 .issuer(issuer)
                 .issueTime(new Date(iat))
                 .expirationTime(new Date(exp))
-                .subject(user.getEmail())
+                .subject(user.getId())
+                .claim("email", user.getEmail())
                 .build();
 
         SignedJWT jwt = new SignedJWT(header, claimsSet);
