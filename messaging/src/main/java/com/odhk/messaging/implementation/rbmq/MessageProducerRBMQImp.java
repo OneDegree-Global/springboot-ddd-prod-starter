2014985d4d10568991b2c3572868eae87b497cf4
Max Chiu <max.chiu@onedegree.hk>
2021-04-16 01:37:11 +0800

    Modify MessageProducerRBMQImp

---
+++ messaging/src/main/java/com/odhk/messaging/implementation/rbmq/MessageProducerRBMQImp.java
@@ -0,0 +1,56 @@
+package com.odhk.messaging.implementation.rbmq;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+import com.odhk.messaging.exceptions.ProtocolIOException;
+import com.odhk.messaging.exceptions.QueueLifecycleException;
+import com.odhk.messaging.implementation.utils.ObjectByteConverter;
+import com.rabbitmq.client.*;
+
+import com.odhk.messaging.*;
+
+public class MessageProducerRBMQImp implements IMessageProducer {
+
+    private Channel channel;
+
+    public MessageProducerRBMQImp() throws ProtocolIOException, QueueLifecycleException {
+        try {
+            this.channel = ChannelFactory.getInstance().getChannel();
+        } catch( IOException | TimeoutException e){
+            throw new QueueLifecycleException(e.toString());
+        }
+    }
+
+    @Override
+    public void send(String queueName, byte[] message) throws ProtocolIOException {
+        try {
+            // Use default exchange if using direct send
+            this.channel.queueDeclare(queueName,false,false,false,null);
+            this.channel.basicPublish("", queueName, MessageProperties.PERSISTENT_TEXT_PLAIN, message);
+        } catch(IOException e){
+            throw new ProtocolIOException(e.toString());
+        }
+    }
+
+    @Override
+    public void send(String queueName, String message) throws ProtocolIOException {
+        try {
+            send(queueName, ObjectByteConverter.encodeObject(message));
+        } catch(ProtocolIOException | IOException e) {
+            throw new ProtocolIOException(e.toString());
+        }
+    }
+
+    @Override
+    public void send(String queueName, Object message) throws ProtocolIOException {
+        byte[] bytes;
+        try {
+            bytes = ObjectByteConverter.encodeObject(message);
+            send(queueName, bytes);
+        } catch(ProtocolIOException | IOException e) {
+            throw new ProtocolIOException(e.toString());
+        }
+    }
+
+}
