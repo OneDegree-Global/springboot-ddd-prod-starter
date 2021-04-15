1cc2d041e549e5a754498512d30cf6361ebdac19
Max Chiu <max.chiu@onedegree.hk>
2021-04-16 01:36:18 +0800

    Add JSONMessage & ObjectByteConverter

--- domain/src/main/java/com/odhk/messaging/messageTypes/JSONMessage.java
+++ domain/src/main/java/com/odhk/messaging/messageTypes/JSONMessage.java
@@ -1,10 +1,20 @@
 package com.odhk.messaging.messageTypes;
 
+import java.io.IOException;
+import java.io.ObjectOutputStream;
 import java.io.Serializable;
+import java.util.Map;
+
+import org.json.JSONObject;
 
 public class JSONMessage implements Serializable{
-    public JSONMessage(){
-        String userName;
-        String password;
-    };
+    public String value;
+    public JSONMessage(JSONObject json){
+        value = json.toString();
+    }
+
+    public JSONObject getJSON(){
+        return new JSONObject(value);
+    }
+
 }
