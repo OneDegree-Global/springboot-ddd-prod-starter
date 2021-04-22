15f6239a08043272a21ba1f6221e4cf16ed8546e
Genchi Lu <genchi.lu@TW168020131M.local>
2021-04-22 11:37:08 +0800

    Integrate web and persistence implement with spring data

--- web/src/main/java/hk/onedegree/web/springboot/Application.java
+++ web/src/main/java/hk/onedegree/web/springboot/Application.java
@@ -1,12 +1,14 @@
 package hk.onedegree.web.springboot;
 
+import hk.onedegree.persistence.rdbms.config.PersistenceUserConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 
-@EnableTransactionManagement
 @SpringBootApplication
+@Import(PersistenceUserConfiguration.class)
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
--- web/src/main/java/hk/onedegree/web/springboot/controller/UserController.java
+++ web/src/main/java/hk/onedegree/web/springboot/controller/UserController.java
@@ -3,8 +3,6 @@ package hk.onedegree.web.springboot.controller;
 import hk.onedegree.application.exception.CreateUserFailsException;
 import hk.onedegree.application.exception.RetrieveUserInfoFailsException;
 import hk.onedegree.application.services.UserService;
-import hk.onedegree.persistence.rdbms.dao.UserDao;
-import hk.onedegree.persistence.rdbms.entities.UserDo;
 import hk.onedegree.web.springboot.controller.error.ErrorCode;
 import hk.onedegree.web.springboot.controller.utils.ResponseUtils;
 import hk.onedegree.web.springboot.dto.User;
@@ -21,11 +19,8 @@ public class UserController {
     @Inject
     UserService userService;
 
-    @Inject
-    UserDao userDao;
 
     @PostMapping("/users")
-    @Transactional("userTransactionManager")
     public ResponseEntity newUser(@RequestBody RegisterRequest registerRequest) throws CreateUserFailsException {
         var result = this.userService.createUser(registerRequest.getEmail(), registerRequest.getPassword());
         if(result.isEmpty()) {
