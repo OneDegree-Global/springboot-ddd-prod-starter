4f6c0d04e772f7ac7a5d3e53a1b890a3fa912068
Genchi Lu <genchi.lu@TW168020131M.local>
2021-04-23 11:18:17 +0800

    Fix wrong type in user dao

--- persistence/src/main/java/hk/onedegree/persistence/rdbms/dao/UserDao.java
+++ persistence/src/main/java/hk/onedegree/persistence/rdbms/dao/UserDao.java
@@ -3,7 +3,9 @@ package hk.onedegree.persistence.rdbms.dao;
 import hk.onedegree.persistence.rdbms.entities.UserDo;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface UserDao extends CrudRepository<UserDo, String> {
-    UserDo findFirstByEmail(String lastName);
-    UserDo findById(long id);
+    UserDo findFirstByEmail(String email);
+    Optional<UserDo> findById(String id);
 }
