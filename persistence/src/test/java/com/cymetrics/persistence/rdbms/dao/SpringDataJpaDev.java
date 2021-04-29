0ad1165c718733af31083c03bd48f0d55e892004
Genchi Lu <genchi.lu@TW168020131M.local>
2021-04-29 17:32:02 +0800

    Add example of jpa

--- persistence/src/test/java/com/cymetrics/persistence/rdbms/dao/SpringDataJpaDev.java
+++ persistence/src/test/java/com/cymetrics/persistence/rdbms/dao/SpringDataJpaDev.java
@@ -1,16 +1,21 @@
 package com.cymetrics.persistence.rdbms.dao;
 
 import com.cymetrics.persistence.rdbms.config.PersistenceUserConfiguration;
+import com.cymetrics.persistence.rdbms.entities.UserDo;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+import java.util.Optional;
+
+@SpringBootApplication
 @SpringBootTest
 @EnableTransactionManagement
 @Import(PersistenceUserConfiguration.class)
-public class Dev {
+public class SpringDataJpaDev {
 
     @Autowired
     private UserDao userDao;
@@ -18,10 +23,9 @@ public class Dev {
 
     @Test
     public void forDev(){
-        // save a few customers
-//        UserDo userDo = userDao.findFirstByEmail("1234567");
-//        System.out.println(userDo);
-//        Optional optional = Optional.ofNullable(userDo);
-//        System.out.println(optional.isEmpty());
+        UserDo userDo = userDao.findFirstByEmail("1234567");
+        System.out.println(userDo);
+        Optional optional = Optional.ofNullable(userDo);
+        System.out.println(optional.isEmpty());
     }
 }
