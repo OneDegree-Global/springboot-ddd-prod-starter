e3a43bef2638920d2529dbbc6b72c23cb1b57a52
Chen Wei Chao <wei.chen@onedegree.hk>
2021-05-04 16:36:17 +0800

    add more display name

--- domain/src/test/java/com/cymetrics/transaction_mail/model/ReceiverTest.java
+++ domain/src/test/java/com/cymetrics/transaction_mail/model/ReceiverTest.java
@@ -3,6 +3,7 @@ package com.cymetrics.transaction_mail.model;
 import com.cymetrics.transaction_mail.exceptions.InvalidEmailFormat;
 import com.cymetrics.transaction_mail.model.Receiver;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.UUID;
@@ -10,6 +11,7 @@ import java.util.UUID;
 public class ReceiverTest {
 
     @Test
+    @DisplayName("If invalid email is provided, corresponding exception should be raised")
     public void construct_invalidEmail_InvalidEmailException() {
         Assertions.assertThrows(InvalidEmailFormat.class, () -> {
            String id = UUID.randomUUID().toString();
@@ -20,6 +22,7 @@ public class ReceiverTest {
     }
 
     @Test
+    @DisplayName("Receiver instance should be created successfully out of correct data")
     public void construct_valid_email() throws InvalidEmailFormat {
         String id = UUID.randomUUID().toString();
         String userName = "Mike Trout";
